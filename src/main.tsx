f9d2e332cc12b85506f058470affd7c28e4332dd
Yilver Andres Uran <yuranh@chefcompany.co>
2025-02-20 20:52:11 -0500

    removing styles import, we dont need it

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-console.log("BACK: ",import.meta.env.VITE_BACKEND_URL)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
